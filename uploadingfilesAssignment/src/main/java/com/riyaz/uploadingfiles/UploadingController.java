36b4cd7495b791993f687bde618091d26485f199
[hiberapp] <[mdriyaz147@gmail.com]>
2017-08-28 14:14:32 -0400

    Final commit

---
+++ uploadingfilesAssignment/src/main/java/com/riyaz/uploadingfiles/UploadingController.java
@@ -0,0 +1,35 @@
+package com.riyaz.uploadingfiles;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+public class UploadingController {
+	public static final String uploadingdir = System.getProperty("user.dir") + "/filesUploadedDir/";
+
+	@RequestMapping("/")
+	public String uploading(Model model) {
+		File file = new File(uploadingdir);
+		model.addAttribute("files", file.listFiles());
+		return "uploading";
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.POST)
+	public String uploadingPost(@RequestParam("uploadingFiles") MultipartFile[] uploadingFiles) throws IOException {
+
+		for (MultipartFile uploadedFile : uploadingFiles) {
+			File file = new File(
+					uploadingdir + uploadedFile.getOriginalFilename() + " size of file in bytes " + uploadedFile.getSize());
+			uploadedFile.transferTo(file);
+		}
+
+		return "redirect:/";
+	}
+}
